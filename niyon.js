e19d011df77b2867ddd9e59b67f3f4a24ad44880
vebradev <justinas@vebra.dev>
2019-05-22 11:23:49 +0300

    Add event listener for burger menu.

--- niyon.js
+++ niyon.js
@@ -1,1 +1,8 @@
 console.log("👋");
+
+const menuBurger = document.querySelector(".menu-burger");
+menuBurger.addEventListener("click", () => {
+  menuBurger.classList.toggle("open");
+  document.querySelector(".menu").classList.toggle("open");
+  document.querySelector("nav").style.background = "#150047";
+});
